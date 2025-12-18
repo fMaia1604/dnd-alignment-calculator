866ac677416e21adda651c72f077a8e976583388
alvesmara <malves@efs.at>
2025-12-18 10:41:45 +0100

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Calculator from './components/Calculator'
+import "./App.css";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <>
-      <h1 style={{ textAlign: 'center', marginTop: '1rem' }}>Calculator</h1>
+      <h1 className="text-3xl font-semibold">Calculator</h1>
       <Calculator />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
